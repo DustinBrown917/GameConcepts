ffe916e8daa92622e65f492e22a66e77087d3e8f
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2018-10-25 21:21:22 -0400

    More stuff

---
+++ Project-Spamgeon/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance_;
+    public static GameManager Instance { get { return instance_; } }
+
+    private static int numOfPlayers_ = 1;
+    public static int NumOfPlayers { get { return numOfPlayers_; } }
+
+    private GameStates currentState;
+
+    public static GameStates GetCurrentState()
+    {
+        if (!IsInitialized()) { return GameStates.INVALID; }
+        return instance_.currentState;
+    }
+
+    public static void ChangeGameState(GameStates state)
+    {
+        if (!IsInitialized()) { return; }
+
+        instance_.lChangeGameState(state);
+    }
+
+
+    /************************************************************************************/
+    /********************************* UNITY BEHAVIOURS *********************************/
+    /************************************************************************************/
+
+    private void Awake()
+    {
+        if(instance_ != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance_ = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /************************************************************************************/
+    /************************************ BEHAVIOURS ************************************/
+    /************************************************************************************/
+
+    private static bool IsInitialized()
+    {
+        if(Instance == null)
+        {
+            Debug.LogError("GameManager not initialized!");
+            return false;
+        }
+        return true;
+    }
+
+    private void lChangeGameState(GameStates state)
+    {
+        if(state == currentState) { return; }
+
+        GameStateChangedArgs args = new GameStateChangedArgs();
+        args.previousState = currentState;
+        args.newState = state;
+
+        currentState = state;
+
+        OnGameStateChanged(args);
+    }
+
+    /************************************************************************************/
+    /************************************** EVENTS **************************************/
+    /************************************************************************************/
+
+    #region GameStateChanged Event
+    public static event EventHandler<GameStateChangedArgs> GameStateChanged;
+
+    public class GameStateChangedArgs : EventArgs
+    {
+        public GameStates previousState;
+        public GameStates newState;
+    }
+
+    private static void OnGameStateChanged(GameStateChangedArgs args)
+    {
+        EventHandler<GameStateChangedArgs> handler = GameStateChanged;
+
+        if(handler != null)
+        {
+            handler(typeof(GameManager), args);
+        }
+    }
+    #endregion
+}
+
+
+public enum GameStates
+{
+    INVALID,
+    START_SREEN,
+    PRE_BATTLE,
+    BATTLE,
+    POST_BATTLE,
+    VICTORY,
+    DEFEAT
+}
---
+++ Project-Spamgeon/Assets/_Scripts/InputGrabber.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputGrabber : MonoBehaviour {
+
+    private static InputGrabber instance_;
+    public static InputGrabber Instance { get { return instance_; } }
+
+    public float SinglePlayerSelectionTime { get { return playerTimers[(int)Players.SINGLE]; } }
+    public float FirstPlayerSelectionTime { get { return playerTimers[(int)Players.FIRST]; } }
+    public float SecondPlayerSelectionTime { get { return playerTimers[(int)Players.SECOND]; } }
+
+    [Header("Player Data"), Tooltip("0 = Single Player, 1 = First Player, 2 = Second Player")]
+    [SerializeField] private float[] playerTimers;
+    [SerializeField] private KeyCode[] playerKeys;
+    private Coroutine[] timerCoroutines;
+    [SerializeField] private float timeToSelect = 1.0f;
+
+    private void Awake()
+    {
+        if(instance_ != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance_ = this;
+
+        playerTimers = new float[3];
+        timerCoroutines = new Coroutine[3];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(playerKeys[0]))
+        {
+            StartSelectionTimer(Players.SINGLE);
+        } else if (Input.GetKeyDown(playerKeys[1]))
+        {
+            StartSelectionTimer(Players.FIRST);
+        } else if (Input.GetKeyDown(playerKeys[2]))
+        {
+            StartSelectionTimer(Players.SECOND);
+        }
+    }
+
+    private void StartSelectionTimer(Players player)
+    {
+        int playerIndex = (int)player;
+        if(timerCoroutines[playerIndex] != null)
+        {
+            StopCoroutine(timerCoroutines[playerIndex]);
+            timerCoroutines[playerIndex] = null;
+        }
+        timerCoroutines[playerIndex] = StartCoroutine(SelectionTimer(player, timerCoroutines[playerIndex]));
+    }
+
+    private IEnumerator SelectionTimer(Players player, Coroutine container)
+    {
+        int playerIndex = (int)player;
+        while (Input.GetKey(playerKeys[playerIndex]) && playerTimers[playerIndex] < timeToSelect)
+        {
+            playerTimers[playerIndex] += Time.deltaTime;
+            yield return null;
+        }
+
+        if(playerTimers[playerIndex] >= timeToSelect)
+        {
+            OnSelectEvent(new SelectEventArgs(player));
+        } else
+        {
+            OnTabEvent(new TabEventArgs(player));
+        }
+        playerTimers[playerIndex] = 0;
+        container = null;
+
+    }
+
+
+
+    /************************************************************************************/
+    /************************************** EVENTS **************************************/
+    /************************************************************************************/
+
+    #region TabEvent Event
+    public event EventHandler<TabEventArgs> TabEvent;
+
+    public class TabEventArgs : EventArgs
+    {
+        public Players player;
+
+        public TabEventArgs(Players p)
+        {
+            player = p;
+        }      
+    }
+
+    private void OnTabEvent(TabEventArgs t)
+    {
+        EventHandler<TabEventArgs> handler = TabEvent;
+
+        if(handler != null)
+        {
+            handler(this, t);
+        }
+
+        Debug.Log("Tab Event for " + t.player.ToString());
+    }
+    #endregion
+
+    #region SelectEvent Event
+    public event EventHandler<SelectEventArgs> SelectEvent;
+
+    public class SelectEventArgs : EventArgs
+    {
+        public Players player;
+
+        public SelectEventArgs(Players p)
+        {
+            player = p;
+        }
+    }
+
+    private void OnSelectEvent(SelectEventArgs s)
+    {
+        EventHandler<SelectEventArgs> handler = SelectEvent;
+
+        if (handler != null)
+        {
+            handler(this, s);
+        }
+
+        Debug.Log("Select Event for " + s.player.ToString());
+    }
+    #endregion
+
+}
+
+public enum Players
+{
+    SINGLE,
+    FIRST,
+    SECOND
+}
